1454f20e484e616c96bfa0e7c9006250fc491456
Vishal <40030063+VishalTharindu@users.noreply.github.com>
2020-10-01 13:23:28 +0530

    string cantaint added

---
+++ src/const/String.js
@@ -0,0 +1,8 @@
+export const String = {
+
+    Join: 'Join Now',
+    invalideEmailAddress: 'Invalide Email Address',
+    PasswordFieldEmpty : 'Password Field cannot be empty',
+    EmailPlaceHolder : 'Email Address',
+    PasswordPlaceHolder : 'Password'
+}
